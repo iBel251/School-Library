13ad4705659e6524a906fb878c15793f32f539ea
iBel251 <ibbimran7@gmail.com>
2022-08-03 10:14:28 +0300

    Decorate a class

---
+++ base.rb
@@ -0,0 +1,28 @@
+require_relative './nameable'
+
+class Base < Nameable
+  def initialize(nameable)
+    @nameable = nameable
+    super(nameable)
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < Base
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Base
+  def correct_name
+    if @nameable.correct_name.length <= 10
+      @nameable.correct_name
+    else
+      @nameable.correct_name[0, 10]
+    end
+  end
+end
