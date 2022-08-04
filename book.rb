977428c906c190e75cd7f37096413da1b2487d85
iBel251 <ibbimran7@gmail.com>
2022-08-04 09:32:44 +0300

    Create basic UI

--- book.rb
+++ book.rb
@@ -1,11 +1,11 @@
 class Book
-    def initialize(title, author)
-      @title = title
-      @author = author
-    end
-    attr_accessor :title, :author
-  
-    def add_rental(date)
-      new.Rental(date)
-    end
+  def initialize(title, author)
+    @title = title
+    @author = author
   end
+  attr_accessor :title, :author
+
+  def add_rental(date)
+    new.Rental(date)
+  end
+end
